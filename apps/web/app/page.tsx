6d2e3612a03adb4612db0f374b3fc76374a45ca4
Santiago Beroch <sberoch@gmail.com>
2025-09-04 12:07:06 -0300

    turborepo + shadcn + linked basic apps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from "@workspace/ui/components/button"
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
+import { Label } from "@workspace/ui/components/label";
 
 export default function Page() {
   return (
@@ -6,7 +8,9 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        <Input placeholder="Input" />
+        <Label>Label</Label>
       </div>
     </div>
-  )
+  );
 }
